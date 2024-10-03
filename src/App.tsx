b288c4552f6df63113e1844fb9061b79273efa8e
Abbas Zareei <abbaszareei1438@gmail.com>
2024-10-03 15:11:38 +0200

    update the platform

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,58 @@
-import { useState } from 'react'
-import './App.css'
-import { Button, ButtonGroup, Grid, GridItem } from '@chakra-ui/react'
-import NavBar from './components/NavBar'
-import GameGrid from './components/GameGrid'
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { useState } from "react";
+import GameGrid from "./components/GameGrid";
+import GameHeading from "./components/GameHeading";
+import GenreList from "./components/GenreList";
+import NavBar from "./components/NavBar";
+import PlatformSelector from "./components/PlatformSelector";
+import SortSelector from "./components/SortSelector";
+import { Platform } from "./hooks/useGames";
+import { Genre } from "./hooks/useGenres";
 
-function App() {
 
-
-  return <Grid templateAreas={`"nav   nav" "aside main"`}>
-    <GridItem area='nav' >
-      <NavBar></NavBar>
-      </GridItem>
-    <GridItem area='aside' >Aside</GridItem>
-    <GridItem area='main' >
-      <GameGrid />
-      </GridItem>
-    </Grid>
+export interface GameQuery { 
+  genre: Genre | null;
+  platform: Platform | null;
+  sortOrder: string;
+  searchText: string;
 }
 
-export default App
+function App() {
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+  
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '250px 1fr'
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })} />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre})} />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <Box paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery} />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform}) } />
+            </Box>
+            <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
+          </Flex>
+        </Box>
+        <GameGrid gameQuery={gameQuery} />
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,41 +1,40 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { px } from "framer-motion";
+import { GameQuery } from "../App";
+import useGames, { Platform } from "../hooks/useGames";
+import { Genre } from "../hooks/useGenres";
 import GameCard from "./GameCard";
-//import { GameQuery } from "../App";
-//import useGames, { Platform } from "../hooks/useGames";
-//import { Genre } from "../hooks/useGenres";
-//import GameCard from "./GameCard";
-//import GameCardContainer from "./GameCardContainer";
-//import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 
-interface Game {
-    id: number;
-    name: string;
+interface Props {
+  gameQuery: GameQuery;
 }
 
-interface FetchGameResponse {
-    count: number;
-    results: Game[]
-}
-const GameGrid = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isLoading } = useGames(gameQuery);
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
-    useEffect(() => {
-        apiClient.get<FetchGameResponse>('./games')
-            .then(res => setGames(res.data.results))
-    })
-    return (
-      <>
-      {error && <Text>{error}</Text>}
-        <SimpleGrid columns={3} spacing={10}>
-            {games.map(game => <GameCard key={game.id} game={game} />)}
-        </SimpleGrid>
-      </>
-    );
-}
+  if (error) return <Text>{error}</Text>;
 
+  return (
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
+  );
+};
 
 export default GameGrid;
