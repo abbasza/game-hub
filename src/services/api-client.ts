2a960c1446b546cc4b7024b8b6065acb4cfdddda
Abbas Zareei <abbaszareei1438@gmail.com>
2024-10-03 15:33:11 +0200

    api update

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '07c30ecf21534e299652e2dc9425470d'
-    }
-})
+        key: "07c30ecf21534e299652e2dc9425470d",
+    },
+});
