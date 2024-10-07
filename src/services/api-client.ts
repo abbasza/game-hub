5697d68e35769971515eed3d75e9f3e136b8a498
Abbas Zareei <107604937+abbasza@users.noreply.github.com>
2024-10-07 11:02:33 +0200

    Update api-client.ts with new key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: "07c30ecf21534e299652e2dc9425470d",
+        key: "496f1d70ff7745ed9223edd8e3dc0122",
     },
 });
